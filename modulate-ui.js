25881f48785b4b5622eadb541260e24f960d9547
bunnie <bunnie@bunniestudios.com>
2016-04-25 00:06:41 +0800

    add control packet types

--- modulate-ui.js
+++ modulate-ui.js
@@ -5,12 +5,15 @@ var ui = {
     
     init: function() {
 	this.sendButton = document.getElementById("send");
+	this.ctrlButton = document.getElementById("ctrl");
 	
 	this.saveButton     = document.getElementById("save");
 	
 	var self = this;
 	this.sendButton.addEventListener("click",
 					 function(e) { self.onModeButton("send"); e.preventDefault(); });
+	this.ctrlButton.addEventListener("click",
+					 function(e) { self.onModeButton("ctrl"); e.preventDefault(); });
 	
 	this.saveButton.addEventListener("click",
 					 function(e) { self.onSaveButton(); e.preventDefault(); });
@@ -40,29 +43,59 @@ var ui = {
 	this.sendButton.setAttribute("selected", "");
 	this.txLed.setAttribute("lit", "");
 
-	var preamble = [00,00,00,00,0xaa,0x55,0x42];
-	var sector = [0x01, 0x80, 0x04]; // version code + two bytes for sector offset
-	// 256 byte payload, preamble, sector offset + 4 bytes hash + 1 byte stop
-	var packetlen = 256 + preamble.length + sector.length + 4 + 1;
-	var buffer = new Uint8Array(packetlen);
-	var i = 0;
-	for( i = 0; i < preamble.length; i++ ) {
-	    buffer[i] = preamble[i];
-	}
-	for( var j = 0; i < sector.length + preamble.length; i++, j++ ) {
-	    buffer[i] = sector[j];
-	}
-	for( ; i < packetlen - 1 - 4; i++ ) {
-	    //buffer[i] = i & 0xff;
-	    buffer[i] = 0x55;
-	}
+	if(mode == "send") {
 
-	hash = murmurhash3_32_gc(buffer.subarray(preamble.length, 256 + preamble.length + sector.length), 0xdeadbeef);
-	buffer[i++] = hash & 0xFF;
-	buffer[i++] = (hash >> 8) & 0xFF;
-	buffer[i++] = (hash >> 16) & 0xFF;
-	buffer[i++] = (hash >> 24) & 0xFF
-	buffer[i] = 0xFF;  // terminate with 0xFF to let last bit demodulate correctly
+	    var preamble = [00,00,00,00,0xaa,0x55,0x42];
+	    var sector = [0x01, 0x80, 0x04]; // version code + two bytes for sector offset
+	    // 256 byte payload, preamble, sector offset + 4 bytes hash + 1 byte stop
+	    var packetlen = 256 + preamble.length + sector.length + 4 + 1;
+	    var buffer = new Uint8Array(packetlen);
+	    var i = 0;
+	    for( i = 0; i < preamble.length; i++ ) {
+		buffer[i] = preamble[i];
+	    }
+	    for( var j = 0; i < sector.length + preamble.length; i++, j++ ) {
+		buffer[i] = sector[j];
+	    }
+	    for( ; i < packetlen - 1 - 4; i++ ) {
+		//buffer[i] = i & 0xff;
+		//buffer[i] = 0x55;
+		buffer[i] = Math.floor((Math.random() * 256));
+	    }
+
+	    hash = murmurhash3_32_gc(buffer.subarray(preamble.length, 256 + preamble.length + sector.length), 0xdeadbeef);
+	    buffer[i++] = hash & 0xFF;
+	    buffer[i++] = (hash >> 8) & 0xFF;
+	    buffer[i++] = (hash >> 16) & 0xFF;
+	    buffer[i++] = (hash >> 24) & 0xFF
+	    buffer[i] = 0xFF;  // terminate with 0xFF to let last bit demodulate correctly
+	} else {
+	    // must be control packet, eh? heheh.
+	    
+	    var preamble = [00,00,00,00,0xaa,0x55,0x42];
+	    var sector = [0x80]; 
+	    var packetlen = 24 + preamble.length + sector.length + 4 + 1;
+	    var buffer = new Uint8Array(packetlen);
+	    var i = 0;
+	    for( i = 0; i < preamble.length; i++ ) {
+		buffer[i] = preamble[i];
+	    }
+	    for( var j = 0; i < sector.length + preamble.length; i++, j++ ) {
+		buffer[i] = sector[j];
+	    }
+	    for( ; i < packetlen - 1 - 4; i++ ) {
+		//buffer[i] = i & 0xff;
+		//buffer[i] = 0x55;
+		buffer[i] = Math.floor((Math.random() * 256));
+	    }
+
+	    hash = murmurhash3_32_gc(buffer.subarray(preamble.length, 24 + preamble.length + sector.length), 0xdeadbeef);
+	    buffer[i++] = hash & 0xFF;
+	    buffer[i++] = (hash >> 8) & 0xFF;
+	    buffer[i++] = (hash >> 16) & 0xFF;
+	    buffer[i++] = (hash >> 24) & 0xFF
+	    buffer[i] = 0xFF;  // terminate with 0xFF to let last bit demodulate correctly
+	}
 	
 	runModem(buffer);
 	
